ee69285df1f751c6ef56d77d09bae209e5860751
Egor Khmelev <hmelyoff@gmail.com>
2022-09-05 13:29:01 +0100

    Rework LedgerPublicKey to handle errors better

--- src/components/LedgerPublicKey.tsx
+++ src/components/LedgerPublicKey.tsx
@@ -1,30 +1,32 @@
 import { Card, DialogContent, DialogContentText, DialogTitle, Icon, IconButton, Stack } from '@mui/material'
 import { Box } from '@mui/system'
 import ClipboardJS from 'clipboard'
-import { PublicKey } from 'near-api-js/lib/utils'
-import { KeyType } from 'near-api-js/lib/utils/key_pair'
 import { useEffect, useRef, useState } from 'react'
-import useLedger from '../hooks/useLedger'
+import { ledgerManager } from '../utils/LedgerManager'
 
 const LedgerPublicKey = () => {
   const dialogContentRef = useRef()
   const [pk, setPk] = useState<string>()
-
-  const { available, getVersion, getPublicKey } = useLedger()
+  const [message, setMessage] = useState("Follow Ledger's on-screen instructions...")
 
   useEffect(() => {
     async function getData() {
-      const pkData = await getPublicKey("44'/397'/0'/0'/1'")
-      if (pkData) {
-        const ledgerPk = new PublicKey({ keyType: KeyType.ED25519, data: pkData })
-        setPk(ledgerPk.toString())
+      try {
+        const publicKey = await ledgerManager.getPublicKey(/*"44'/397'/0'/0'/1'"*/)
+        if (publicKey) {
+          setPk(publicKey.toString())
+        }
+      } catch (err: any) {
+        setMessage(err.message)
       }
     }
 
-    if (available) {
-      getData()
+    getData()
+
+    return () => {
+      ledgerManager.disconnect().catch(() => {})
     }
-  }, [available, getVersion, getPublicKey])
+  }, [])
 
   return (
     <>
@@ -35,7 +37,9 @@ const LedgerPublicKey = () => {
             <DialogContentText>Copy and send Public Key to Multisig Manager:</DialogContentText>
             <Card variant="outlined">
               <Stack direction="row" justifyContent="space-between" alignItems="center">
-                <Box sx={{ p: 2, fontFamily: 'monospace', overflow: 'hidden', textOverflow: 'ellipsis' }}>{pk}</Box>
+                <Box sx={{ p: 2, fontFamily: 'monospace', overflow: 'hidden', textOverflow: 'ellipsis' }}>
+                  {pk ?? message}
+                </Box>
                 <IconButton onClick={handleCopyClick}>
                   <Icon fontSize="small" color="secondary" className="material-symbols-outlined">
                     content_copy
